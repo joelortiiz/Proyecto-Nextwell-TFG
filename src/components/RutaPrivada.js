f944a8a711aab28f247e0ca649f3fd6c8db9f5b4
joelortiiz <daw_joelortiz@riberadeltajo.es>
2024-05-16 09:45:10 +0200

    Private routes fixed

--- src/components/RutaPrivada.js
+++ src/components/RutaPrivada.js
@@ -3,9 +3,10 @@ import { useAtuh } from '../context/AuthContext'
 import { Navigate } from 'react-router-dom'
 
 export const RutaPrivada = ({children}) => {
-const usuario = useAtuh()
-
-if(usuario){
+    debugger
+const usuario = useAtuh().usuario
+console.log(usuario)
+if(usuario !== null){
     return children
 } else {
     return <Navigate replace to='/iniciar-sesion' />
