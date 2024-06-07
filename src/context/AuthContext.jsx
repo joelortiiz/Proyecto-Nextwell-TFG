c18c6c2e305bac0565ac042bbd6b846f5576d0e4
joelortiiz <daw_joelortiz@riberadeltajo.es>
2024-06-07 18:14:58 +0200

    comments in  context.js

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,32 +1,41 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../firebase/firebaseConfig';
+import React, { useState, useContext, useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth'; 
+import { auth } from '../firebase/firebaseConfig'; 
 
+// Crea el contexto de autenticación
 const AuthContext = React.createContext();
 
-//Hook to acess to the context 
-
+// Hook personalizado para acceder al contexto de autenticación
 const useAtuh = () => {
-    return useContext(AuthContext)
+    return useContext(AuthContext); // Usa useContext para acceder al valor del AuthContext
 }
 
+// Componente proveedor de autenticación
 const AuthProvider = ({ children }) => {
-    const [usuario, setUsuario] = useState();
-    const [load, setLoading] = useState(true);
-
+    const [usuario, setUsuario] = useState(); 
+    // Estado para almacenar el usuario autenticado
+    const [load, setLoading] = useState(true); 
+    // Estado para manejar el estado de carga
 
     useEffect(() => {
+        // Suscribirse a los cambios de estado de autenticación
         const logOut = onAuthStateChanged(auth, (usuario) => {
-            setUsuario(usuario)
-            setLoading(false)
+            setUsuario(usuario); 
+            // Actualiza el estado del usuario cuando cambia el estado de autenticación
+            setLoading(false); 
+            // Establece el estado de carga como falso una vez que se verifica el estado del usuario
         });
-        return logOut
+        return logOut; 
+        // Limpia el observador cuando el componente se desmonta
     }, []);
+
     return (
+        // Provee el contexto de autenticación a los componentes hijos
         <AuthContext.Provider value={{ usuario: usuario }}>
-            {!load && children}
+            {!load && children} {/* Renderiza los hijos sólo cuando la carga ha finalizado */}
         </AuthContext.Provider>
-    )
+    );
 }
 
-export { AuthProvider, AuthContext, useAtuh }
+// Exporta el proveedor de autenticación, el contexto y el hook personalizado
+export { AuthProvider, AuthContext, useAtuh };
